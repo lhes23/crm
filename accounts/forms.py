b1a7cf6e502242e41f0ff6a5ce4deb6dac9b9252
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-08-28 17:36:07 +0800

    add the styling to the User Register Template
    remove the class 'form-control' on forms.py and add it to the html
    because the password can't add the class without removing the auth messages
    used form.password.html_name

--- accounts/forms.py
+++ accounts/forms.py
@@ -31,14 +31,3 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
-        widgets = {
-            'username':forms.TextInput(attrs={
-                'placeholder':'Username',
-                'class':'form-control',
-            }),
-            'email':forms.TextInput(attrs={
-                'placeholder':'Email Address',
-                'class':'form-control',
-            }),
-            'password1':forms.PasswordInput(attrs={'class':'form-control'}),
-        }
